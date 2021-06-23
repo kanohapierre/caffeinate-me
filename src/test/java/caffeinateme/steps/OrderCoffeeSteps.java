06d615abd9d90de64eff3f6e1055c7c1f18ab591
John Ferguson Smart <john.smart@wakaleo.com>
2021-06-24 00:06:25 +0100

    Sample solution

--- src/test/java/caffeinateme/steps/OrderCoffeeSteps.java
+++ src/test/java/caffeinateme/steps/OrderCoffeeSteps.java
@@ -1,21 +1,24 @@
 package caffeinateme.steps;
 
-import caffeinateme.model.CoffeeShop;
-import caffeinateme.model.Customer;
-import caffeinateme.model.Order;
-import caffeinateme.model.OrderStatus;
+import caffeinateme.model.*;
+import io.cucumber.java.DataTableType;
 import io.cucumber.java.en.And;
 import io.cucumber.java.ParameterType;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
+import java.util.List;
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class OrderCoffeeSteps {
     Customer cathy = Customer.named("Cathy");
-    CoffeeShop coffeeShop = new CoffeeShop();
+    ProductCatalog productCatalog = new ProductCatalog();
+    CoffeeShop coffeeShop = new CoffeeShop(productCatalog);
     Order order;
+    Receipt receipt;
 
     @Given("Cathy is {int} metre(s) from the coffee shop")
     public void cathy_is_metres_from_the_coffee_shop(Integer distanceInMetres) {
@@ -26,6 +29,20 @@ public class OrderCoffeeSteps {
     public void cathy_orders_a(String orderedProduct) {
         this.order = Order.of(1,orderedProduct).forCustomer(cathy);
         cathy.placesAnOrderFor(order).at(coffeeShop);
+    }
+
+    @DataTableType
+    public OrderItem mapRowToOrderItem(Map<String, String> entry) {
+        return new OrderItem(entry.get("Product"),
+                Integer.parseInt(entry.get("Quantity")));
+    }
+
+    @Given("^Cathy has ordered:$")
+    public void sarahHasOrdered(List<OrderItem> orders) {
+        for(OrderItem item : orders) {
+            Order order = Order.of(item.getQuantity(),item.getProduct()).forCustomer(cathy);
+            cathy.placesAnOrderFor(order).at(coffeeShop);
+        }
     }
 
     @Then("Barry should receive the order")
@@ -49,4 +66,45 @@ public class OrderCoffeeSteps {
     public void customerIsMinutesAway(int etaInMinutes) {
         coffeeShop.setCustomerETA(cathy, etaInMinutes);
     }
+
+    @DataTableType
+    public ProductPrice mapRowToProductPrice(Map<String, String> entry) {
+        return new ProductPrice(entry.get("Product"), Double.parseDouble(entry.get("Price")));
+    }
+
+    @Given("^the following prices:$")
+    public void theFollowingPrices(List<ProductPrice> productPrices) throws Throwable {
+        productCatalog.addProductsWithPrices(productPrices);
+    }
+
+    @When("^she asks for a receipt$")
+    public void sheAsksForAReceipt() {
+        receipt = coffeeShop.getReceiptFor(cathy);
+    }
+
+
+    @DataTableType
+    public ReceiptLineItem mapRowToReceiptLineItem(Map<String, String> entry) {
+        return new ReceiptLineItem(entry.get("Product"),
+                Integer.parseInt(entry.get("Quantity")),
+                Double.parseDouble(entry.get("Price")));
+    }
+
+    @Then("^she should receive a receipt totalling:$")
+    public void sheShouldReceiveAReceiptTotalling(List<Map<String, String>> receiptTotals) {
+        Double serviceFee = Double.parseDouble(receiptTotals.get(0).get("Service Fee"));
+        Double subtotal = Double.parseDouble(receiptTotals.get(0).get("Subtotal"));
+        Double total = Double.parseDouble(receiptTotals.get(0).get("Total"));
+
+        assertThat(receipt.getServiceFee()).isEqualTo(serviceFee);
+        assertThat(receipt.getSubtotal()).isEqualTo(subtotal);
+        assertThat(receipt.getTotal()).isEqualTo(total);
+    }
+
+    @And("^the receipt should contain the line items:$")
+    public void theReceiptShouldContainTheLineItems(List<ReceiptLineItem> expectedLineItems) throws Throwable {
+        assertThat(receipt.getLineItems()).containsExactlyElementsOf(expectedLineItems);
+    }
+
+
 }
